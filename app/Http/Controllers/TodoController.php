c456a32169e644ede913a7991f789da3916b0a9a
sydney.desousa <sydney.desousa@mirumagency.com>
2020-08-13 17:20:11 +0200

    initial commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('todos');
+    }
+
+    public function fetch()
+    {
+        return Todo::all(['id','name']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $todo = Todo::create($request->all());
+
+        return $todo;
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return 'deleted';
+    }
+}
---
+++ public/assets/js/todos.js
@@ -0,0 +1,105 @@
+/**
+ * create the main constants
+ */
+/*****************************
+ *   CONSTANT DECLARATIONS
+ ****************************/
+const addForm = document.querySelector('form.add');     // form to enter a new todo
+const list = document.querySelector('.todos')           // ul containing all the todos
+const searchForm = document.querySelector('form.search');   // form to search for a todo
+
+/*****************************
+ *    Method expressions
+ ****************************/
+const listTodos = () => {
+
+    axios
+    .get('/todos/all')
+    .then( response => {
+        const todos = response.data;
+        todos.forEach( todo => addTodo(todo.name, todo.id));
+    })
+    .catch(err => console.log(err));
+};
+
+/**
+ * 
+ * @param {*} todo 
+ * @param {*} id . Used to set the data-target. This serves as reference during an update/delete process
+ */
+const addTodo = (todo, id = null) => {
+    const html = `
+        <li data-target="${id}" class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+    list.innerHTML += html;
+};
+
+// this is what runs when the page loads
+listTodos();
+
+/***************SUBMISSION METHODS****************/
+
+/**
+ * Deleting a todo
+ */
+list.addEventListener('click', e => {
+
+    console.log(e);
+    const ID = e.target.parentElement.getAttribute('data-target'); // this is has the item ID
+    const targetedList = e.target;                                 // this is the element we clicked
+
+    // check if we clicked on the delete icon
+    if(targetedList.classList.contains('delete')) {
+
+        axios
+        .delete(`todo/${ID}/delete`)
+        .then(res => {
+            if(res.statusText === 'OK'){
+                targetedList.parentElement.remove();
+            }
+        })
+        .catch(err => alert("Something went wrong. Possible API connection issue"));
+    }
+});
+
+/**
+ * Add new todo
+ * when the user enter a todo in the form field
+ * we must get the text that is in the input and put in a varialble
+ * this same text is what will go to the database;
+ */
+addForm.addEventListener('submit', function(e) {
+
+    e.preventDefault();         
+
+    axios
+    .post('/todo/store', { name: this.add.value.trim() })
+    .then( response => {
+        if(response.statusText == "Created"){
+            addTodo(response.data.name, response.data.id);
+            addForm.reset(); // clear the form fields
+        } 
+    })
+    .catch(err => console.log(err));
+
+});
+
+
+/**
+ * Search
+ */
+searchForm.addEventListener('keyup', e => {
+
+    const inputText = e.target.value.toLowerCase().trim(); // get the input text
+    
+    const listItems = Array.from(list.children);// get all ul lists and convert to an array
+
+    listItems.map(item => item.textContent.toLowerCase().trim().includes(inputText) ? item.classList.remove('filtered') : item.classList.add('filtered'));
+
+});
+
+
+
